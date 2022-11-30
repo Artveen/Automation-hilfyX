a11f010855195fb2b4b67f69dde66ec68884aab9
Artem Ivankov <artem.ivankov.95@gmail.com>
2022-11-30 16:18:32 +0200

    booking page and case added

--- cypress/e2e/X-Web/bookRA.cy.js
+++ cypress/e2e/X-Web/bookRA.cy.js
@@ -1,16 +1,11 @@
-/// <reference types="cypress"/>
-it("Book Remote assist",() => {
-    cy.visit('https://web.stage.dopomo.com.ua/cabinet');
-    cy.get('input.form-control').type('962261815');
-    cy.get('button.btn.btn_green.btn_h54').click();
-    cy.get('.form__container .form__item:nth-child(1) .form__input').type(1);
-    cy.get('.form__container .form__item:nth-child(2) .form__input').type(8);
-    cy.get('.form__container .form__item:nth-child(3) .form__input').type(1);
-    cy.get('.form__container .form__item:nth-child(4) .form__input').type(5);
-    // .form__container .form__item:nth-child(1) .form__input how to find first input
-    cy.get('button.btn.btn_green.btn_h54').click();
-    cy.get('[class="page page_bg-pink"] .menu__option:nth-child(2)') .click();
-    cy.get('.time-slot_active') .click();
-    cy.get('[class="btn btn_green btn_h54"]') .click();
+import bookRemoteAssistPage from "../../support/pages/webClient/bookRemoteAssistPage";
+import user from "../../fixtures/webClient/user.json"
 
+describe("Test suite for booking remote assist",()=>{
+describe("Positive scenarios",()=>{
+    it("Book remote assist",()=>{
+    bookRemoteAssistPage.visit();
+    bookRemoteAssistPage.createOrderRemoteAssist(user.phoneNumber, user.code1, user.code2, user.code3, user.code4)
+    })
+})
 })
