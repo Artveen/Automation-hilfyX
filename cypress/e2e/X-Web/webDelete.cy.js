fad05f99cb121096f8d4ce32ecee53ba7f33332c
Artem Ivankov <artem.ivankov.95@gmail.com>
2022-11-30 14:58:16 +0200

    delete negative case added

--- cypress/e2e/X-Web/webDelete.cy.js
+++ cypress/e2e/X-Web/webDelete.cy.js
@@ -1,11 +1,17 @@
+import 'cypress-iframe'
 import webDeletePage from "../../support/pages/webClient/webDeletePage";
 import user from "../../fixtures/webClient/user.json"
+import cardNumber from "../../fixtures/webClient/cardNumber.json"
 
 describe ("Test suite for delete account", ()=>{
 describe("Posititve scenarios",()=>{
- it("Delete account with zero balance",()=>{
+ it.skip("Delete account with zero balance",()=>{
     webDeletePage.visit();
-    webDeletePage.DeleteAccountWithZeroBalance(user.phoneNumber, user.code1, user.code2, user.code3, user.code4);
+    webDeletePage.deleteAccountWithZeroBalance(user.phoneNumber, user.code1, user.code2, user.code3, user.code4);
+ })
+ it("Delete account with negative balance + payment",()=>{
+    webDeletePage.visit();
+    webDeletePage.deleteAccountWithNEgativeBalancePayment(user.phoneNumber, user.code1, user.code2, user.code3,user.code4, cardNumber.cardNumber, cardNumber.date, cardNumber.cvc)
  })
 })
 })
--- cypress/support/pages/webClient/webDeletePage.js
+++ cypress/support/pages/webClient/webDeletePage.js
@@ -1,39 +1,52 @@
+import 'cypress-iframe'
 class webDeletePage{
     visit(){
         cy.visit('https://web.stage.dopomo.com.ua/cabinet')
     }
     getPhoneNumberField(){
-        return cy.get('input.form-control')
+        return cy.get('input.form-control');
     
     }
     getStartButton(){
-        return cy.get('button.btn.btn_green.btn_h54')   
+        return cy.get('button.btn.btn_green.btn_h54');   
     } 
     getVerificationField1(){
-       return cy.get('.form__container .form__item:nth-child(1) .form__input')
+       return cy.get('.form__container .form__item:nth-child(1) .form__input');
     }
     getVerificationField2(){
-        return cy.get('.form__container .form__item:nth-child(2) .form__input')
+        return cy.get('.form__container .form__item:nth-child(2) .form__input');
     }
     getVerificationField3(){
-        return cy.get('.form__container .form__item:nth-child(3) .form__input')
+        return cy.get('.form__container .form__item:nth-child(3) .form__input');
     }
     getVerificationField4(){
-        return cy.get('.form__container .form__item:nth-child(4) .form__input')
+        return cy.get('.form__container .form__item:nth-child(4) .form__input');
     }
     getSubmitButton(){
-        return cy.get('button.btn.btn_green.btn_h54')
+        return cy.get('button.btn.btn_green.btn_h54');
     }
     getProfile(){
-        return cy.get('[class="menu__center"] [class="font-bold foz-14"]')
+        return cy.get('[class="menu__center"] [class="font-bold foz-14"]');
     }
     getDeleteProfileButton(){
-        return cy.get('[class="btn_delete"]') 
+        return cy.get('[class="btn_delete"]');
     }
     getSubmitDeleteButton(){
-        return   cy.get('[class="ant-btn ant-btn-primary"]')
+        return   cy.get('[class="ant-btn ant-btn-primary"]');
     }
-    DeleteAccountWithZeroBalance(phoneNumber, code1, code2, code3, code4){
+    getStripeButton(){
+        return cy.get('.btn.btn_stripe');
+    }
+    getPaymentWindow(){
+        return cy.frameLoaded('iframe[title="Secure payment input frame"]');
+    }
+    getStripeWindow(){
+        return cy.iframe('iframe[title="Secure payment input frame"]');
+    }
+    getSubmitPaymentButton(){
+        return cy.get('[id="submit"]');
+    }
+    deleteAccountWithZeroBalance(phoneNumber, code1, code2, code3, code4){
         cy.log("Delete account with zero balance")
         this.getPhoneNumberField().type(phoneNumber);
         this.getStartButton().click();
@@ -43,5 +56,33 @@ class webDeletePage{
         this.getVerificationField4().type(code4);
         this.getSubmitButton().click()
         this.getProfile().click()
+        this.getDeleteProfileButton().click();
+        this.getSubmitButton().click();
         }
+    deleteAccountWithNEgativeBalancePayment(phoneNumber, code1, code2, code3, code4, cardNumber, date, cvc){
+        cy.log("Delete account with negative balance + payment")
+        this.getPhoneNumberField().type(phoneNumber);
+        this.getStartButton().click();
+        this.getVerificationField1().type(code1);
+        this.getVerificationField2().type(code2);
+        this.getVerificationField3().type(code3);
+        this.getVerificationField4().type(code4);
+        this.getSubmitButton().click();
+        this.getProfile().click();
+        this.getDeleteProfileButton().click();
+        this.getStripeButton().click();
+        this.getStripeWindow().find('[id="Field-numberInput"]').type(cardNumber);
+        this.getStripeWindow().find('[id="Field-expiryInput"]').type(date);
+        this.getStripeWindow().find('[id="Field-cvcInput"]').type(cvc);
+        this.getSubmitPaymentButton().click();
+    }
     } export default new webDeletePage
+
+
+    
+
+
+
+
+
+
