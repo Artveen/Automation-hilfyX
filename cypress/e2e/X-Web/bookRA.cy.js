1507d49c73b258ee1bef9627a0b0947184f5dbed
Artem Ivankov <artem.ivankov.95@gmail.com>
2022-12-02 14:05:28 +0200

    refactoring

--- cypress/e2e/X-Web/bookRA.cy.js
+++ cypress/e2e/X-Web/bookRA.cy.js
@@ -1,13 +1,26 @@
 import bookRemoteAssistPage from "../../support/pages/webClient/bookRemoteAssistPage";
 import webAuthorizationPage from '../../support/pages/webClient/webAuthorizationPage'
 import user from "../../fixtures/webClient/user.json"
+import promoCodes from "../../fixtures/webClient/promoCodes.json"
+
+let token;
 
 describe("Test suite for booking remote assist",()=>{
 describe("Positive scenarios",()=>{
     it("Book remote assist",()=>{
     bookRemoteAssistPage.visit();
     webAuthorizationPage.submitLoginForm(user.phoneNumber, user.code1, user.code2, user.code3, user.code4);
+    cy.get('[class="page page_bg-pink"] .menu__option:nth-child(2)').should('be.visible') .then(()=>{
+    cy.getCookie('access_token') .then(cookie =>{
+    token = cookie.value
+    })  
     bookRemoteAssistPage.createOrderRemoteAssist()
     })
+    it("Booking with referal promo",()=>{
+    bookRemoteAssistPage.visit();
+    cy.setCookie('access_token', token);
+    bookRemoteAssistPage.createRemoteAssistWithReferalPromo(promoCodes.referal)
+    })
+})
 })
 })
--- cypress/support/pages/webClient/bookRemoteAssistPage.js
+++ cypress/support/pages/webClient/bookRemoteAssistPage.js
@@ -8,9 +8,19 @@ class bookingPageRemoteAssist {
     getTimePicker(){
         return cy.get('.time-slot_active')
     }
+    getPromoCodeWindow(){
+        return cy.get ('[class="btn btn_grey btn_h54"]')
+    }
+    getPromoCodeField(){
+        return cy.get('[class="form__input form__input_uppercase "]')
+    }
+    applyPromoCode(){
+        return cy.get('[class="form-item"]')
+    }
     submitBooking(){
         return cy.get('[class="btn btn_green btn_h54"]') 
     }
+    
     createOrderRemoteAssist(){
         cy.log("Create order remote assist")
         this.getSubmitButton().click();
@@ -18,4 +28,14 @@ class bookingPageRemoteAssist {
         this.getTimePicker().click();
         this.submitBooking().click();
         }
+    createRemoteAssistWithReferalPromo(referal){
+        cy.log("Create remote assist with referal promo")
+        this.getSubmitButton().click();
+        this.getBookingPage().click();
+        this.getTimePicker().click();
+        this.getPromoCodeWindow().click();
+        this.getPromoCodeField().type(referal);
+        this.applyPromoCode().click();
+        this.submitBooking().click();
+    }
 } export default new bookingPageRemoteAssist
